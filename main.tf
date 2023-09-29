3da2c191806aa7aad4ee59f8159a01a6907051c7
yunaumov <yunaumov@sweb.ru>
2023-09-29 17:08:53 +0300

    upd load task

--- main.tf
+++ main.tf
@@ -5,12 +5,25 @@ terraform {
       source = "selectel/selectel"
       version = "4.0.1-alpha"
     }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "1.51.0"
+    }
   }
 }
 
 provider "selectel" {
-    domain_name = "id"
-    username = "user"
-    password = "pass"
-    token = "token"
+  domain_name = "<id>"
+  username = "<uname>"
+  password = "<pass>"
+  token = "<token>"
 }
+
+provider "openstack" {
+  auth_url = "https://api.selvpc.ru/identity/v3"
+  domain_name = "<id>"
+  tenant_id = "<proj_id>"
+  user_name = "<uname>"
+  password = "<pass>"
+  region = "<pool>"
+}  
