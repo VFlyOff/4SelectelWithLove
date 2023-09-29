3970a07419f8e9c52470f3ef2be8e33f0366ba21
yunaumov <yunaumov@sweb.ru>
2023-09-29 17:37:10 +0300

    upd quest

--- quest.tf
+++ quest.tf
@@ -9,6 +9,10 @@ resource "selectel_vpc_project_v2" "remote-exec" {
 
   provisioner "remote-exec" {
     inline = [
+      "apt-get upgrade -y && apt-get update -y"
+      "apt-get install -y nginx"
+      "systemctl enable nginx.service && systemctl start nginx.service"  
+      "echo '<h1>Iurii Naumov / <a href='https://github.com/VFlyOff/4SelectelWithLove'>GitHub</a></h1>' > /var/www/html/index.nginx-debian.html"
       "echo '<h3>Open ports:' >> /var/www/html/index.nginx-debian.html",
       "echo `ss -tulpn | awk '{print $5}' | tail -n +2 | sed 's|.*:||' | sort | uniq -c | awk '{print $2}'` >> /var/www/html/index.nginx-debian.html",
       "echo '</h3>' >> /var/www/html/index.nginx-debian.html"
