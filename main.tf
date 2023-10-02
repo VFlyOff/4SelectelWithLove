a06b240b41eae89b72b35af0970f117bcdc14132
yunaumov <yunaumov@sweb.ru>
2023-10-02 17:36:04 +0300

    last V

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.49.0"
+      version = "~> 1.51.0"
     }
     selectel = {
       source = "selectel/selectel"
