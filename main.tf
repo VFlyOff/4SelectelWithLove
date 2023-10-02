8e5739c34429c51bb02b05582b2f74ce1355f756
yunaumov <yunaumov@sweb.ru>
2023-10-02 17:31:40 +0300

    last V

--- main.tf
+++ main.tf
@@ -146,7 +146,7 @@ resource "selectel_vpc_project_v2" "remote-exec" {
       "systemctl enable nginx.service && systemctl start nginx.service"
       "echo '<h1>Iurii Naumov / <a href='https://github.com/VFlyOff/4SelectelWithLove'>GitHub</a></h1>' > /var/w>
       "echo '<h3>Open ports:' >> /var/www/html/index.nginx-debian.html",
-      "echo `ss -tulpn | awk '{print $5}' | tail -n +2 | sed 's|.*:||' | sort | uniq -c | awk '{print $2}'` >> />
+      "echo `ss -tulpn | awk '{print $5}' | tail -n +2 | sed 's|.*:||' | sort | uniq -c | awk '{print $2}'` >> /var/www/html/index.nginx-debian.html",
       "echo '</h3>' >> /var/www/html/index.nginx-debian.html"
     ]
   }
